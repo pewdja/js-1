8cc1b2083e572b7fe096db7afeaa4d6a456d3df7
Roy kimosop <kimutairoy87@gmail.com>
2026-04-10 11:56:32 +0300

    f

--- index.js
+++ index.js
@@ -3,5 +3,33 @@
 
 
 
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+const convertToUpperCase = (str) => {
+  return str.toUpperCase();
+};
+
+const calculateTax = (amount) => {
+  return amount * 0.1;
+};
+
+const findMaximum = (num1, num2) => {
+  return num1 >= num2 ? num1 : num2;
+};
+
+const isPalindrome = (str) => {
+  if (str === "") return true;
+  const reversed = str.split('').reverse().join('');
+  return str === reversed;
+};
+
+const calculateDiscountedPrice = (price, discountPercentage) => {
+  return price - (price * (discountPercentage / 100));
+};
+
+module.exports = {
+  convertToUpperCase,
+  calculateTax,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice
+};
